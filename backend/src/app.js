38e4138200f590e006dbe7cf921fc4dd203c1f76
Abdurazaq333 <irazaqr986@gmail.com>
2024-02-27 13:55:51 +0500

    connect with cloudinary

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,6 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json({ limit: "16kb" }));
+
 export { app };
---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinaryUpload = async (filePath) => {
+  try {
+    !filePath && null; // if not file path return null
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "image", // type of image define
+    });
+    //file has been uploaded succesfully
+    console.log(`file has been succesfully uploaded ${response.url}`);
+    fs.unlinkSync(filePath);
+    return result;
+  } catch (error) {
+    fs.unlinkSync(filePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { cloudinaryUpload };

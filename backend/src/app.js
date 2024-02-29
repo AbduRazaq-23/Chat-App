15adeb6fe4ee36af8d06f23086671c6ba204f612
Abdurazaq333 <irazaqr986@gmail.com>
2024-02-29 23:51:51 +0500

    all configuration are ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,4 +4,10 @@ const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,1 +1,9 @@
-c;
+import { asyncHandler } from "../utils/asyncHander.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "May teak hu jani",
+  });
+});
+
+export default registerUser;

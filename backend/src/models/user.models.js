aff8f7d5f2fd3120d9302ff5684a2ed26128c87e
Abdurazaq333 <irazaqr986@gmail.com>
2024-03-03 14:16:55 +0500

    configuration in user model

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userModel = new mongoose.Schema(
   {
@@ -16,9 +17,36 @@ const userModel = new mongoose.Schema(
     },
     avatar: {
       type: String,
+      required: true,
+    },
+    token: {
+      type: String,
     },
   },
   { timestamps: true }
 );
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userModel.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userModel.methods.generateToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.TOKEN_SECRET,
+    {
+      expireIn: process.env.TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userModel);

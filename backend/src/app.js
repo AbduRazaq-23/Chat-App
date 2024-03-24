615230d5eee07beabc2041e8e4edb298ebb55d4c
Abdurazaq333 <irazaqr986@gmail.com>
2024-03-25 00:11:02 +0500

    remove the bodyparser

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,18 +3,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
 
 //import routes
 import userRouter from "./routes/user.routes.js";
 import chatRouter from "./routes/chat.routes.js";
-import bodyParser from "body-parser";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
